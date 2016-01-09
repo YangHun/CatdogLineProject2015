f8152eb177fd509c1f43910f5b0ab6c72e3adf24
ventusmagus <ventusmagus@gmail.com>
2016-01-10 01:58:08 +0900

    Merge two sam and modified unused sam codes

---
+++ Assets/Scripts/Sections/Lv1_Section1/ObjectSam.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System;
+
+public class ObjectSam : ObjectData, IInteractor {
+	
+	public HealType m_Type = HealType.BLUE;
+
+    private bool m_IsPlayerInRange = false;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject == GameManager.Inst().GetPlayer())
+        {
+            m_IsPlayerInRange = true;
+            Debug.Log("Sam : Player In Range");
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject == GameManager.Inst().GetPlayer())
+        {
+            m_IsPlayerInRange = false;
+            Debug.Log("Sam : Player Out of Range");
+        }
+    }
+
+    public void OnInteractStart()
+    {
+        GameManager.Inst().SetPlayerHealType(m_Type);
+    }
+
+    public void OnInteractStay()
+    {
+        GameManager.Inst().StopInteraction();
+    }
+
+    public void OnInteractEnd()
+    {
+        // do nothing
+    }
+
+    public bool IsInteractable()
+    {
+        return m_IsPlayerInRange;
+    }
+}
