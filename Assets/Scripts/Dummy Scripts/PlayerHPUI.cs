15df2ea6fcb00b141751a2f527f4c1e23142a213
Ezest <bsh04054@naver.com>
2016-01-12 15:28:10 +0900

    Player HP

---
+++ Assets/Scripts/Dummy Scripts/PlayerHPUI.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerHPUI : MonoBehaviour {
+
+    public PlayerController Player;
+    public Image HPbar;
+	void Update ()
+    {
+        HPbar.rectTransform.localScale = new Vector3(Player.GetHP() / Player.GetMaxHP(), 1, 1);
+	}
+}
