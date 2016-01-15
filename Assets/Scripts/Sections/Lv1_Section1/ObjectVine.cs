c92c8f04f17fc1598737496d93c3d8cb6c9a6ec1
Ezest <bsh04054@naver.com>
2016-01-16 00:36:23 +0900

    change vine script

--- Assets/Scripts/Sections/Lv1_Section1/ObjectVine.cs
+++ Assets/Scripts/Sections/Lv1_Section1/ObjectVine.cs
@@ -4,21 +4,36 @@ using System.Collections;
 public class ObjectVine : UnitData, IInteractor, IHealable {
 
     private HealType m_Type = HealType.GREEN;
-    [SerializeField] private GameObject player;
-    [SerializeField] private Rigidbody2D playerRigid;
+    //[SerializeField] private GameObject player;
+    //private Rigidbody2D playerRigid;
+    //private Collider2D col;
+    //[SerializeField]
+    //private Colliding down;
+    //[SerializeField]
+    //private Colliding up;
     private bool m_IsPlayerInRange = false;
-    private bool NowInterAct = false;
-    void Update ()
-    {
-        if (NowInterAct)
-            StartCoroutine("MoveUp");
-        if (!m_IsPlayerInRange)
-        {
-            NowInterAct = false;
-            playerRigid.gravityScale = 3.0f;
-        }
-            
-    }
+    private bool climbing = false;
+//    private bool NowInterAct = false;
+//    private int GoUp = 1;
+    //void Start()
+    //{
+    //    playerRigid = player.GetComponent<Rigidbody2D>();
+    //    col = player.GetComponent<Collider2D>();
+    //}
+    //
+    //void Update ()
+    //{
+    //    if (NowInterAct)
+    //    {
+    //        playerRigid.velocity = new Vector3(0, 5.0f * GoUp, 0);
+    //    }
+    //    if (!m_IsPlayerInRange)
+    //    {
+    //        NowInterAct = false;
+    //        playerRigid.gravityScale = 3.0f;
+    //        col.isTrigger = false;
+    //    }
+    //}
 
     void OnTriggerEnter2D(Collider2D other)
     {
@@ -34,8 +49,9 @@ public class ObjectVine : UnitData, IInteractor, IHealable {
         if (other.gameObject == GameManager.Inst().GetPlayer())
         {
             m_IsPlayerInRange = false;
-            NowInterAct = false;
+            //NowInterAct = false;
             Debug.Log("Vine : Player Out of Range");
+            climbing = false;
         }
     }
 
@@ -43,20 +59,28 @@ public class ObjectVine : UnitData, IInteractor, IHealable {
     {
         if (m_IsPlayerInRange)
         {
-            NowInterAct = true;
-            playerRigid.gravityScale = 0.0f;
+            climbing = true;
+            //NowInterAct = true;
+            //playerRigid.gravityScale = 0.0f;
+            //col.isTrigger = true;
+            //if (down.IsCollided())
+            //    GoUp = 1;
+            //else if (up.IsCollided())
+            //    GoUp = -1;
+            //else
+            //    GoUp *= -1;
         }
             
     }
 
     public void OnInteractStay()
     {
-
+        GameManager.Inst().StopInteraction();
     }
 
     public void OnInteractEnd()
     {
-
+        //do nothing
     }
 
     public bool IsInteractable()
@@ -64,12 +88,6 @@ public class ObjectVine : UnitData, IInteractor, IHealable {
         if (IsHealthy() && m_IsPlayerInRange)
             return true;
         return false;
-    }
-
-    IEnumerator MoveUp()
-    {
-        player.transform.position += new Vector3(0, 0.1f, 0);
-        yield return null;
     }
 
     public void OnHealed(HealInfo heal)
@@ -83,5 +101,10 @@ public class ObjectVine : UnitData, IInteractor, IHealable {
     public bool IsHealable()
     {
         return true;
+    }
+
+    public bool IsClimbing()
+    {
+        return climbing;
     }
 }
