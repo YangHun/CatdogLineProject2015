2592a1b8910d7562b393e6de2e44ce994c5428be
Ezest <bsh04054@naver.com>
2016-01-17 01:20:30 +0900

    Section 2

---
+++ Assets/Scripts/Sections/Lv1_Section2/Section2.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Section2 : Section
+{
+    [SerializeField]
+    private Collider2D Block;
+
+    public override void OnSectionStart()
+    {
+        IInteractor[] tmp = GetComponentsInChildren<IInteractor>();
+        GameManager.Inst().RefreshInteractorList(tmp);
+        Block.gameObject.SetActive(true);
+    }
+
+    public override void OnSectionPause()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void OnSectionResume()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void OnSectionEnd()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override float GetSectionScore()
+    {
+        return 0;
+    }
+}
