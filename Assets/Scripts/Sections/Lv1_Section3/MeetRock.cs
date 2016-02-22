fad46f8c47075d1181e140e79525afcb0b51c490
Ezest <bsh04054@naver.com>
2016-02-23 02:08:41 +0900

    save

---
+++ Assets/Scripts/Sections/Lv1_Section3/MeetRock.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MeetRock : MonoBehaviour {
+
+    [SerializeField]
+    private ObjectHunter_a Hunter;
+    [SerializeField]
+    private GameObject Rock1, Rock2;
+    private bool a = false;
+    private float t = 2.0f;
+	void Update () {
+	    if (a)
+        {
+            t -= GameTime.deltaTime;
+        }
+        if (a && t <= 0)
+        {
+            Hunter.rigid.velocity = new Vector2(0, 15f);
+            a = false;
+            Hunter.Move = true;
+        }
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject == Rock1 || other.gameObject == Rock2)
+        {
+            Hunter.Move = false;
+            a = true;
+            t = 2.0f;
+        }
+    }
+}
--- Assets/Scripts/Sections/Lv1_Section3/ObjectHunter_a.cs
+++ Assets/Scripts/Sections/Lv1_Section3/ObjectHunter_a.cs
@@ -3,13 +3,33 @@ using System.Collections;
 
 public class ObjectHunter_a : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+    private float WaitTime = 0.0f;
+    public bool Move = false;
+    public Rigidbody2D rigid;
+    private float ReloadTime = 3.0f;
+    private bool Reload = false;
+    private bool FirstJump = false;
+    void Start () {
+        rigid = this.GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+	    if (WaitTime > 0)
+            WaitTime -= GameTime.deltaTime;
+        if (WaitTime <= 0 && !Move)
+            Move = true;
+        if (Move)
+        {
+            if (this.transform.position.x <= 128.5 && !FirstJump)
+            {
+                rigid.velocity = new Vector2(0, 20f);
+                FirstJump = true;
+            }
+            this.transform.position = new Vector2(this.transform.position.x - 0.05f, this.transform.position.y);
+            if (rigid.velocity.x < 0)
+                rigid.velocity = new Vector2(0, 0);
+         //   rigid.AddForce(new Vector2(0.02f, 0));
+        }
 	}
 }
